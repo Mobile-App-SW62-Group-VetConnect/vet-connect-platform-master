cd06cabc14a5af39eed646865e4a6c40e20e95f7
Carlos Chávez <u201910317@upc.edu.pe>
2024-11-19 16:02:45 -0500

    Review updated

--- src/main/java/com/org/vetconnect/platform/profiles/domain/model/entities/Review.java
+++ src/main/java/com/org/vetconnect/platform/profiles/domain/model/entities/Review.java
@@ -8,38 +8,40 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
 @Entity
 public class Review extends AuditableModel {
 
     @Id
-    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     // un vet center puede tener varias reviews
     // ESTO DEBE SER CAMBIADO POR USERID
     @ManyToOne
-    @Getter
     @JoinColumn(name="pet_owner_id", nullable = false)
-    @JoinColumn(name="pet_owner_name", nullable = false)
     private PetOwner petOwner;
+
+
+
+    @JoinColumn(name="pet_owner_name", nullable = false)
+    private String petOwnerName;
 
 
 
     // una review solo puede pertenecer a un pet owner
     @ManyToOne
-    @Getter
     @JoinColumn(name="vet_center_id", nullable = false)
     private VetCenter vetCenter;
 
-    @Getter
     private String comments;
 
-    @Getter
     @Min(1)
     @Max(5)
     private int rating;
@@ -51,6 +53,7 @@ public class Review extends AuditableModel {
 
     public Review(PetOwner petOwner, VetCenter vetCenter, String comments, int rating) {
         this.petOwner = petOwner;
+        this.petOwnerName = petOwner.getName();
         this.vetCenter = vetCenter;
         this.comments = comments;
         this.rating = rating;
