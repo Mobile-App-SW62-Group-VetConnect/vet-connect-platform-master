5dd32e2f0267daad79cde66b03c4c930f4615495
Carlos Chávez <u201910317@upc.edu.pe>
2024-11-20 07:44:34 -0500

    Update OpenApiConfiguration.java

--- src/main/java/com/org/vetconnect/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/org/vetconnect/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -7,10 +7,11 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfiguration {
@@ -41,6 +42,8 @@ public class OpenApiConfiguration {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
+        openApi
+                .servers(List.of(new Server().url("https://vet-connect-platform-master-production-de97.up.railway.app")));
         return openApi;
     }
 }
